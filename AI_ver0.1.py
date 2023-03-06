e3fddf51c932fa7aac70b7ed8443e6f889a91fcd
r3dg0d <87044255+r3dg0d@users.noreply.github.com>
2023-03-06 10:10:08 -0800

    Create AI_ver0.1.py
    Used ChatGPT to cheat.

---
+++ AI_ver0.1.py
@@ -0,0 +1,76 @@
+# This version of SIVA Barebones was fixed by ChatGPT with a single prompt.
+
+import pyttsx3
+import speech_recognition as sr
+import time
+import random
+
+sr.__version__ = '3.8.1'
+
+print("Hello, world!")
+print("I'm SIVA, an Intelligent, Kind, and helpful information assistant.")
+print("loading code... please wait...")
+
+def talk(text, lang="en-US", voice="en-US", sample_rate=16000, device="default"):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    engine.setProperty('voice', voice)
+    engine.say(text)
+    engine.runAndWait()
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    talk("Feel free to talk to your device.")
+
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print(f"You said: {text}")
+        return text
+    except sr.UnknownValueError:
+        talk("Sorry, I could not understand what you said. Please try again.")
+    except sr.RequestError as e:
+        talk("Sorry, could not process your request at the moment. Please try again later.")
+        print(f"Error: {e}")
+    return ""
+
+# Commands for SIVA (Super Intelligent Voice assistant)
+def run_siva():
+    talk("How can I assist you?")
+
+    while True:
+        text = recognize_speech().lower()
+        if text == "power on":
+            talk("Powering on.")
+        elif text == "power off":
+            talk("Powering off.")
+        elif text == "what am i looking at":
+            talk("Sorry, I cannot answer that question as I do not have access to your device's camera.")
+        elif text == "what am i going to":
+            talk("I am not sure. Could you please specify?")
+        elif text == "play music":
+            talk("Sorry, I do not have access to your device's music library.")
+        elif text == "stop music":
+            talk("Sorry, I do not have access to your device's music library.")
+        elif text.startswith("search for"):
+            query = text[10:]
+            talk(f"Searching for {query}.")
+        elif text == "convert video to 3d model":
+            talk("Sorry, I do not have the necessary tools to perform this task.")
+        elif text.startswith("video call"):
+            args = text[11:].split()
+            if len(args) != 2:
+                talk("Sorry, could not recognize the name and location of the person you want to call.")
+            else:
+                name, location = args
+                talk(f"Calling {name} at {location}.")
+        elif text == "goodbye":
+            talk("Goodbye! Have a nice day.")
+            break
+        else:
+            talk("Sorry, I did not understand what you said. Please try again.")
